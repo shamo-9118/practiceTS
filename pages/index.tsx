0837b9b56444543193cad2c71bfa7804f189796f
shamo-9118 <won9118391183@gmail.com>
2022-10-31 22:27:33 +0900

    🍻 container queries を試してみた

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,29 @@ import Head from "next/head";
 import { NextPage } from "next";
 
 type HeadChildren = {
-  [key:string]:{}
+  [key: string]: {};
 };
 
 const Home: NextPage = (props: HeadChildren) => {
-  console.log(props);
+  let obj1: object = [];
+  let obj2: { a: { b: string } } = { a: { b: "aaa" } };
 
   return (
     <>
-      {props.children}
-      <div>hello</div>
+      <div>
+        <div className="flex flex-col gap-4 p-4">
+          <div className="@container w-[200px]">
+            <div className="bg-green-300 @[300px]:bg-gray-300 p-8">
+              Container Queries適用
+            </div>
+          </div>
+          <div className="w-[300px]">
+            <div className="bg-yellow-300 p-8">
+              300pxの要素
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
