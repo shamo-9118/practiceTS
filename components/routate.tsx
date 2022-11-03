c234875e65b38a7682d17f72ccd86b95b0062741
shamo-9118 <won9118391183@gmail.com>
2022-11-03 21:49:21 +0900

    うんちプリっ

--- components/routate.tsx
+++ components/routate.tsx
@@ -1,8 +1,9 @@
 export const Rotate = () => {
     return (
         <div>
+            <label htmlFor=""></label>
             <div
-                className="m-10 border-box w-[30px] h-[30px] border-4 border-r-indigo-500 border-b-indigo-500
+                id="" className="m-10 border-box w-[30px] h-[30px] border-4 border-r-indigo-500 border-b-indigo-500
             border-t-white border-white duration-300 rotate-45 hover:rotate-[225deg] origin-center"
             ></div>
             <div className="w-[100px] h-[100px] bg-gray-500 m-[0_auto] duration-300 hover:skew-y-[-12deg]"></div>
