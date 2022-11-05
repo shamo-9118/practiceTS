7b5e69a4c4b7265786076c6591fb5c55db86a692
shamo-9118 <won9118391183@gmail.com>
2022-11-05 17:22:40 +0900

    🍻 二重円のアニメーション追加してみた

---
+++ components/DualRoundLoader/DualRoundLoader.tsx
@@ -0,0 +1,10 @@
+import classes from "./DualRoundLoader.module.css";
+
+export const DualRoundLoader = () => {
+    return (
+        <div className={classes.RoundLoader}>
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
