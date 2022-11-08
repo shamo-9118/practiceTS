c237cbc3630bac7b6c90f076e764e185152b54c8
shamo-9118 <won9118391183@gmail.com>
2022-11-08 22:53:03 +0900

    🍻 reduceメソッドを理解する！

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -1,8 +1,35 @@
 import { CharAnimation } from "../components/CharAnimation/CharAnimation";
 import { Menu } from "../components/Menu/Menu";
 import { ImgAnimation } from "../components/ImgAnimation/ImgAnimation";
+import { log } from "console";
 
 const menu = () => {
+    // const hello =(callback:any,name:string)=>{
+    //     console.log(callback);
+    //     console.log("hello" + callback(name));
+    // }
+    // hello((person:string)=>{
+    //     return  person;
+    // },"マイケル")
+
+    const str = "animation";
+    const strArry = str.split("");
+
+    const result = strArry.reduce((accu, curr) => {
+        console.log(accu)
+        console.log(`${accu} <${curr}>`);
+        return `${accu} <${curr}>`;
+    }, "");
+    console.log(result);
+
+    // const arry =["a","b","c","d","e"]
+
+    // const result = arry.reduce(function(acuu, curr) {
+    //     console.log(acuu,curr)
+    //     return  curr + acuu
+    // });
+    // console.log(result)
+
     return (
         <div className="bg-cyan-200 absolute left-0 right-0">
             <Menu></Menu>
