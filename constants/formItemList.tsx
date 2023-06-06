335dc3e19967c2b09f1267f8315f6815f63d961a
shamo-9118 <won9118391183@gmail.com>
2023-06-07 06:41:09 +0900

    固定のアイテムを別ファイルに移動させました

---
+++ constants/formItemList.tsx
@@ -0,0 +1,19 @@
+export const formItemList = [
+  {
+    label: '名前',
+    placeholder: '山田太郎',
+    type: 'text',
+    name: 'username',
+  },
+  {
+    label: 'メールアドレス',
+    placeholder: 'example@example.com',
+    type: 'text',
+    name: 'mailAddress',
+  },
+  {
+    label: 'お問い合わせ内容',
+    type: 'text',
+    name: 'password',
+  },
+] as const
